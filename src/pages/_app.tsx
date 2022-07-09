38e24b1a11e928c52e83258addbd6fe73ff99d04
Yuya Furusawa <fullyou0798@gmail.com>
2022-07-10 02:46:31 +0900

    Headの追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
 import { AppProps } from 'next/app';
 
 import 'styles/global.scss';
@@ -7,6 +8,9 @@ import styles from './_app.module.scss';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
+    <Head>
+      <title>Anki-Cards</title>
+    </Head>
     <nav className={styles.header}>
       <div className="container">
         <Link href="/">
